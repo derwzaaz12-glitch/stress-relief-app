55ec4362267554500f96292a49abb1b854b935ae
derwzaaz12-glitch <derwzaaz12@gmail.com>
2026-03-17 12:36:24 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+export default function App() {
+  const [page, setPage] = useState(1);
+  return <div>Relax App Ready 🚀</div>;
+}
